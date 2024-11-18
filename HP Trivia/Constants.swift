b3236710de1f138a366c462d9aa2d9476fd4118c
Karla <karlamiriam6@gmail.com>
2024-11-18 14:49:05 -0300

    Initial commit

---
+++ HP Trivia/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HP Trivia
+//
+//  Created by Karla Miriam Dos santos Goncalves on 17/11/24.
+//
+
+import Foundation
+
+enum Constants {
+    static let hpFont = "PartyLetPlain"
+}
---
+++ HP Trivia/Instructions.swift
@@ -0,0 +1,18 @@
+//
+//  Instructions.swift
+//  HP Trivia
+//
+//  Created by Karla Miriam Dos santos Goncalves on 18/11/24.
+//
+
+import SwiftUI
+
+struct Instructions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Instructions()
+}
